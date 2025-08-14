e478a86b286d5addbe8985456b4154899aa88433
Jason Morley <hello@jbmorley.co.uk>
2025-08-14 10:16:39 -1000

    test: Add simple extraction test (#146)
    It’s about time. This is preparatory work for updating the version of
    OpoLua we use. It introduces the basics of a test framework and a single
    SIS file extraction test. This is currently expected to fail as a result
    of #114.

---
+++ tests/test_extraction.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import tempfile
+import unittest
+
+ROOT_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TOOLS_DIRECTORY = os.path.join(ROOT_DIRECTORY, "tools")
+EXAMPLES_DIRECTORY = os.path.join(ROOT_DIRECTORY, "examples")
+
+sys.path.append(TOOLS_DIRECTORY)
+
+import indexer
+
+from indexer import import_installer
+
+
+class ExtractionTests(unittest.TestCase):
+
+    @unittest.expectedFailure
+    def test_import_installer(self):
+
+        errors = []
+        def error_handler(error):
+            errors.append(error)
+
+        with tempfile.TemporaryDirectory() as temporary_directory:
+            installer = import_installer(source={},
+                                         output_directory=temporary_directory,
+                                         reference=[],
+                                         path=os.path.join(EXAMPLES_DIRECTORY, "watchdog.SIS"),
+                                         error_handler=error_handler)
+        installer_dictionary = installer.as_dict(relative_icons_path="icons")
+        self.assertEqual(installer_dictionary["version"], "1.06")
+
+
+if __name__ == "__main__":
+    unittest.main()
